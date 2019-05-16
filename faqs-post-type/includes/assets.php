9dbec09d9c56cf4a3367e84cad9c28e7ae02ffed
Bob O'Brien <45097853+bobdoesdev@users.noreply.github.com>
2019-05-16 14:26:08 -0400

    Update assets.php

--- faqs-post-type/includes/assets.php
+++ faqs-post-type/includes/assets.php
@@ -8,9 +8,10 @@ if( in_array( 'faqs-post-type/faqs-post-type.php', apply_filters( 'active_plugin
 } 
 
 function styles_and_scripts()  {
-    wp_enqueue_style( 'faq.css', plugin_dir_url(__DIR__)  . 'assets/css/main.min.css', array(), '1', 'all' );
-    wp_enqueue_script('masonry-js', 'https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js', array( 'jquery' ),'4.2.2', true);
-    wp_enqueue_script( 'faq.js', plugin_dir_url(__DIR__)  . 'assets/js/faq.js', array('jquery', 'masonry-js'), '1', 'all', true );
+	if (is_post_type_archive( 'faq' ) ){
+	    wp_enqueue_style( 'faq.css', plugin_dir_url(__DIR__)  . 'assets/css/main.min.css', array(), '1', 'all' );
+		wp_enqueue_script('masonry-js', 'https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js', array( 'jquery' ),'4.2.2', true);
+		wp_enqueue_script( 'faq.js', plugin_dir_url(__DIR__)  . 'assets/js/faq.js', array('jquery', 'masonry-js'), '1', 'all', true );
+	}
 }
-
 
